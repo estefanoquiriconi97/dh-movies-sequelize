dcf0f68e74daade9b9b30cca20d940f47f5ea883
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-01-24 11:58:41 -0300

    serie detail

--- src/controllers/seriesController.js
+++ src/controllers/seriesController.js
@@ -3,19 +3,24 @@ const db = require("../database/models");
 const seriesController = {
   list: async (req, res) => {
     try {
-
       const series = await db.Series.findAll({
-        include : ["genre"]
+        include: ["genre"],
       });
-      res.render('series/list', {series});
-
-    } catch (error) {console.error(error)}
+      res.render("series/list", { series });
+    } catch (error) {
+      console.error(error);
+    }
   },
 
   detail: async (req, res) => {
     try {
-
-    } catch (error) {console.error(error)}
+      const serie = await db.Series.findByPk(req.params.id,{
+        include : ["genre"]
+      });
+      res.render("series/detail", { serie });
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
 
